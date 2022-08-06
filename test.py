2e01b49b45bc3e2d9128e8b8fb85863a051c8c36
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-08-06 16:06:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 #import Bio
 import numpy as np
 import requests
-from Bio.Seq import Seq 
+from bio.Seq import Seq 
 #import biopython
 #seq = Seq("AGCT") 
 st.success("success")
