8def130c6fceb738230f916e919808b7b639158b
AlexeyNesterovichEst <64419626+AlexeyNesterovichEst@users.noreply.github.com>
2022-08-06 16:03:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import streamlit as st
 #import Bio
 import numpy as np
 import requests
-import Bio
-#from Bio.Seq import Seq 
+from Bio.Seq import Seq 
 #import biopython
 #seq = Seq("AGCT") 
 st.success("success")
